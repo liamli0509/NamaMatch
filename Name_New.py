15fed1ea1f461a35b43d51cf9b20afa6b69d5139
liamli0509 <30735257+liamli0509@users.noreply.github.com>
2018-08-01 16:42:01 -0400

    Update Name_New.py

--- Name_New.py
+++ Name_New.py
@@ -19,23 +19,18 @@ import nltk
 from nltk.util import ngrams # This is the ngram magic.
 import os
 
-####################################################################################
-Oracle = cx_Oracle.connect('unidb/fnRjn5LLuuZLAJLQLuQr@amz-p-ora20.dbrs.local:1521/BRAPP')
-os.chdir('U:/ECB/Coverage')
+
 #####################################################################################
 
-ECB_Rated = pd.read_csv('ea_csv_180723_rated.csv',encoding='utf-8', sep='\t')
-ECB_Name = ECB_Rated['ISSUER_NAME'].tolist()
-ECB_Name = list(set(ECB_Name))
+DataSet1 = pd.read_csv(filename1,encoding='utf-8', sep='\t')
+Name1 = DataSet1['ISSUER_NAME'].tolist()
+Name1 = list(set(ECB_Name))
 
-query='''select COMPANY_ID, NAME from COMPANY where IS_ACTIVE='Y' order by COMPANY_ID'''
-DBRS_Data = pd.read_sql(query, Oracle)
-DBRS_Name = DBRS_Data['NAME'].tolist()
+DataSet2 = pd.read_csv(filename2,encoding='utf-8', sep='\t')
+Name2 = DataSet2['ISSUER_NAME'].tolist()
+Name2 = list(set(ECB_Name))
 
-#ECB = pd.read_csv('ECB.txt',encoding='utf-8', sep='\t')
-#ECB_Name = ECB['ECB_ISSUER_NAME'].tolist()
-#DBRS = pd.read_csv('DBRS.txt',encoding='utf-8', sep='\t')
-#DBRS_Name = DBRS['DBRS_ISSUER_NAME'].tolist()
+
 
 
 
@@ -49,20 +44,18 @@ splitter_naive = lambda x: re_sent_ends_naive.split(re_stripper_naive.sub(' ', x
 
 sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
 
-def name_breaker(txt):
-    """Get tuples that ignores all punctuation (including sentences)."""
+def name_breaker(txt):#break down name by ngrams
     if not txt: return None
     ng = ngrams(re_stripper_alpha.sub(' ', txt).split(), NGRAM)
     return list(ng)
 
 
-def jaccard_distance(a, b):
-    """Calculate the jaccard distance between sets A and B"""
+def jaccard_distance(a, b):#compute the jaccard distance between two names
     a = set(a)
     b = set(b)
     return 1.0 * len(a&b)/len(a|b)
 
-def cosine_similarity_ngrams(a, b):
+def cosine_similarity_ngrams(a, b):#compute the cosine similarity between two names
     vec1 = Counter(a)
     vec2 = Counter(b)
     
@@ -77,7 +70,7 @@ def cosine_similarity_ngrams(a, b):
         return 0.0
     return float(numerator) / denominator
 
-def main(name, namelist):
+def main(name, namelist):#compare name from dataset1 to all names in dataset2, get the best match
     scoreList = []
     name = name.lower().replace('.','')
     a = name_breaker(name)
@@ -96,14 +89,14 @@ def main(name, namelist):
 
 
 output = []
-print('%s names to match'%len(ECB_Name))
-for name in ECB_Name:
-    best_DBRS_Name, best_score = main(name, DBRS_Name)
+print('%s names to match'%len(Name1))
+for name in Name1:
+    best_DBRS_Name, best_score = main(name, Name2)
     match = (name, best_DBRS_Name, best_score)
     output.append(match)
 output_df = pd.DataFrame(output)
 
 
-writer = pd.ExcelWriter('ECB_Matched.xlsx')
+writer = pd.ExcelWriter('Name_Matched.xlsx')
 output_df.to_excel(writer, index=False)
 writer.save()
